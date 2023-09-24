007e1d8b8601f28867e253ba4751c6a14cc87414
Alexander Nieves <paulalexander.nieves@gmail.com>
2023-09-24 13:08:52 -0500

    chore: Some practice

---
+++ your_order.js
@@ -0,0 +1,9 @@
+function order(words){
+    numbers=[]
+    array=words.split(" ")
+    const findNumber = words.match(/[1-9]/g);
+
+    return findNumber
+  }
+
+console.log(order("is2 Thi1s T4est 3a"))
